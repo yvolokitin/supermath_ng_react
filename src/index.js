3089c858fe9b37abc8ac823924e5e5bea80cb88a
Yura Volokitin <yuri.volokitin@gmail.com>
2019-10-08 17:23:15 +0200

    added under source control
    added under source control

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SMAlbum from './SMAlbum';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SMAlbum />, document.getElementById('supermath_app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
