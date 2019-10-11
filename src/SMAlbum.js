49cc581ef3398a0521d32617ec8a74fe77d4781a
Yura Volokitin <yuri.volokitin@gmail.com>
2019-10-11 16:10:41 +0200

    merged latest changes

--- src/SMAlbum.js
+++ src/SMAlbum.js
@@ -1,5 +1,4 @@
 ﻿import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,19 +6,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
-import HomeIcon from '@material-ui/icons/Home';
-import IconButton from '@material-ui/core/IconButton';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-
-import SMHelpCarousel from './SMHelpCarousel';
+import SMHeader from "./SMHeader";
 
 const useStyles = makeStyles(theme => {
   return ({
@@ -63,28 +55,9 @@ export default function SMAlbum() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            <IconButton color="inherit" className={classes.button}>
-              <HomeIcon />
-            </IconButton>
-            <IconButton color="inherit" className={classes.button}>
-              <InfoOutlinedIcon />
-            </IconButton>
-             <IconButton color="inherit" className={classes.button}>
-              <HelpOutlineIcon />
-            </IconButton>
-            <IconButton color="inherit" className={classes.button}>
-              <PersonOutlineIcon />
-            </IconButton>
-          </Typography>
-          <Button color="inherit" style={{alignItems: 'flex-end', flexDirection: 'row', justifyContent: "flex-end"}}>Login</Button>
-        </Toolbar>
-      </AppBar>
+      <SMHeader login={"Login"}/>
 
       <main>
-        {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm" >
             <Typography variant="h1" align="center" color="textSecondary" style={{fontFamily: "Grinched", fontVariant: "small-caps", color: "green" }} paragraph>
