50d5b213224f9e2d96915239fadee3806e4dd347
Yura Volokitin <yuri.volokitin@gmail.com>
2019-12-07 00:24:09 +0100

    merged changes

---
+++ src/components/games/circles.js
@@ -0,0 +1,42 @@
+﻿import React from 'react';
+
+/*
+    tbd: somehow via array in Reactjs
+    const circles = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+*/
+export default function SMCircles(props) {
+    return (
+        <>
+            <svg height="100" width="100">
+                <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+            </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="green" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="orange" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="orange" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="red" />
+                        </svg>
+                        <svg height="100" width="100">
+                            <circle cx="50" cy="50" r="17" stroke="black" stroke-width="1" fill="red" />
+                        </svg>
+        </>
+    );
+}
--- src/components/games/simplegame.js
+++ src/components/games/simplegame.js
@@ -2,6 +2,9 @@
 import {Dialog, DialogTitle, DialogContent, Paper, Typography} from '@material-ui/core';
 
 import SMKeyBoard from "./../keyboard/keyboard";
+import SMCircles from "./circles";
+
+import './simplegame.css';
 
 export default class SMSimpleGame extends React.Component {
     constructor(props) {
@@ -18,102 +21,32 @@ export default class SMSimpleGame extends React.Component {
         const operator = target.innerText;
     }
 
+    /*
+        https://about.phamvanlam.com/calculator/
+    */
     render() {
-        const titleStyle = {
-            fontFamily: "Grinched",
-            color: "green",
-        };
-        const taskNumber = {
-            fontFamily: "Grinched",
-            color: "black",
-            textAlign: 'right',
-        };
-        const taskNumberUnderline = {
-            fontFamily: "Grinched",
-            textDecoration: 'underline',
-            color: "black",
-            textAlign: 'right',
-        };
-        const taskQuestion = {
-            fontFamily: "Grinched",
-            color: "light grey",
-            textAlign: 'right',
-        };
-        const paperStyle = {
-            height: '100%',
-            backgroundColor: 'white',
-        };
-
-        const wrapper = {
-            width: '100%',
-            height: '100%',
-        };
-        const topDiv = {
-            padding: '20px',
-            width: '100%',
-        };
-        const leftHalfDiv = {
-            width: '49%',
-            height: '100%',
-            float: 'left',
-            border: '3px solid green',
-        }
-        const rightHalfDiv = {
-            width: '49%',
-            height: '100%',
-            float: 'right',
-            border: '3px solid red',
-
-            display:'flex',
-            justifyContent:'center',
-            alignItems:'center',
-        }
-
-        /*
-            this.props.open
-            https://about.phamvanlam.com/calculator/
-        */
-/*
-        return (
-        <Dialog onClose={() => this.props.onClick()} fullScreen={true} open={'true'}>
-            <DialogTitle style={{backgroundColor: '#99ccff'}}>
-                <Typography component={'div'} variant="h4" style={titleStyle}>SUPERMATH</Typography>
-            </DialogTitle>
-
-            <DialogContent style={{ backgroundColor: 'orange' }}>
-                <Paper>
-                    <div style={leftHalfDiv}>
-                        <Typography variant="h1" style={taskNumber}>2</Typography>
-                        <Typography variant="h1" style={taskNumberUnderline}>+    2</Typography>
-                        <Typography variant="h1" style={taskQuestion}>?</Typography>
-                    </div>
-                    <div style={rightHalfDiv}>
-                        <SMKeyBoard onDigit={this.onDigit} onOperator={this.onOperator}/>
-                    </div>
-                </Paper>
-
-                <Paper>
-
-                </Paper>
-
-            </DialogContent>
-
-        </Dialog>
-        );
-*/
         return (
             <Dialog onClose={() => this.props.onClick()} fullScreen={true} open={'true'}>
-                <div style={wrapper}>
-                    <div style={topDiv}>
-                        <Typography component={'div'} variant="h4" style={titleStyle}>SUPERMATH</Typography>
+                <div className="wrapper">
+                    <div className="header_div">
+                        <div className="header_div_left">SUPERMATH</div>
+                        <div className="header_div_right">smile</div>
                     </div>
 
-                    <div style={leftHalfDiv}>
-                        TBD...2+2=4
+                    <div className="body_div">
+                        <div className="body_div_left">
+                            <div className="gameplay">
+                                TBD...2+2=4
+                            </div>
+                        </div>
+
+                        <div className="body_div_right">
+                            <SMKeyBoard onDigit={this.onDigit} onOperator={this.onOperator}/>
+                        </div>
                     </div>
 
-                    <div style={rightHalfDiv}>
-                        <SMKeyBoard onDigit={this.onDigit} onOperator={this.onOperator}/>
+                    <div className="footer_div">
+                        <SMCircles />
                     </div>
                 </div>
             </Dialog>
