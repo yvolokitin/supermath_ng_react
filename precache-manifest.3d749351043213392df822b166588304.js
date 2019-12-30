fff2a907b0f50b9e8943c08013d948cb6e4893fc
Yura Volokitin <yuri.volokitin@gmail.com>
2019-12-30 22:35:32 +0100

    Updates

---
+++ precache-manifest.3d749351043213392df822b166588304.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bc7e7c6ec22f58d009923ad24e6c44",
+    "url": "/supermath_ng_react/index.html"
+  },
+  {
+    "revision": "aa2e4b22979ef4dec178",
+    "url": "/supermath_ng_react/static/css/main.b1949f1d.chunk.css"
+  },
+  {
+    "revision": "ae82ba33f50d636f2ffe",
+    "url": "/supermath_ng_react/static/js/2.be5aa49f.chunk.js"
+  },
+  {
+    "revision": "2eafd76b430ecb83f7df483a44fe70e5",
+    "url": "/supermath_ng_react/static/js/2.be5aa49f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa2e4b22979ef4dec178",
+    "url": "/supermath_ng_react/static/js/main.29475a40.chunk.js"
+  },
+  {
+    "revision": "7071020c75ba51977fee",
+    "url": "/supermath_ng_react/static/js/runtime-main.5654d705.js"
+  },
+  {
+    "revision": "f4968790972219ee3eabe771c462b38d",
+    "url": "/supermath_ng_react/static/media/Grinched2.0.f4968790.ttf"
+  },
+  {
+    "revision": "79bce60dda885b399a45f23a848db81c",
+    "url": "/supermath_ng_react/static/media/vitamins.79bce60d.jpg"
+  },
+  {
+    "revision": "cbfe1cd25c30fdb17f4bec6c459b3625",
+    "url": "/supermath_ng_react/static/media/white_10.cbfe1cd2.jpg"
+  },
+  {
+    "revision": "bb65407cbcaf3735b5bbd53b1cff09f6",
+    "url": "/supermath_ng_react/static/media/white_11.bb65407c.jpg"
+  },
+  {
+    "revision": "f7dc0622c07e4feca0cac8517664b8e9",
+    "url": "/supermath_ng_react/static/media/white_2.f7dc0622.jpg"
+  },
+  {
+    "revision": "5eab40369cc1f27f3a9c1a8d659dabd6",
+    "url": "/supermath_ng_react/static/media/white_3.5eab4036.jpg"
+  },
+  {
+    "revision": "1935530dcbd521be54202b8f7f671142",
+    "url": "/supermath_ng_react/static/media/white_4.1935530d.jpg"
+  },
+  {
+    "revision": "5b8fea688003a4daf127da2ac216cecc",
+    "url": "/supermath_ng_react/static/media/white_6.5b8fea68.jpg"
+  },
+  {
+    "revision": "de89801caa68475cc2b9593c458de9f1",
+    "url": "/supermath_ng_react/static/media/white_7.de89801c.jpg"
+  },
+  {
+    "revision": "f958f5f431f6670df900e4bf529c5fb8",
+    "url": "/supermath_ng_react/static/media/white_8.f958f5f4.jpg"
+  },
+  {
+    "revision": "151f44cf513ecf22d1726422bae2106f",
+    "url": "/supermath_ng_react/static/media/white_9.151f44cf.jpg"
+  }
+]);
