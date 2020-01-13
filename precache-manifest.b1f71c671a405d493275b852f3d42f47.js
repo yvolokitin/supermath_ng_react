1b417d31160d3388ef9ca41a65ea42fc994fb98b
Yura Volokitin <yuri.volokitin@gmail.com>
2020-01-13 03:04:04 +0100

    Updates

---
+++ precache-manifest.b1f71c671a405d493275b852f3d42f47.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d692c7dc0bb0a78c69ce291ae57d5664",
+    "url": "/supermath_ng_react/index.html"
+  },
+  {
+    "revision": "a516782af2aa85714cc6",
+    "url": "/supermath_ng_react/static/css/main.4e0f86f3.chunk.css"
+  },
+  {
+    "revision": "f458d535d9bfa31f3906",
+    "url": "/supermath_ng_react/static/js/2.9e080b9c.chunk.js"
+  },
+  {
+    "revision": "2eafd76b430ecb83f7df483a44fe70e5",
+    "url": "/supermath_ng_react/static/js/2.9e080b9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a516782af2aa85714cc6",
+    "url": "/supermath_ng_react/static/js/main.47b42fa2.chunk.js"
+  },
+  {
+    "revision": "9b5b6bbb9acd37f31293",
+    "url": "/supermath_ng_react/static/js/runtime-main.4df7c6a2.js"
+  },
+  {
+    "revision": "f4968790972219ee3eabe771c462b38d",
+    "url": "/supermath_ng_react/static/media/Grinched2.0.f4968790.ttf"
+  },
+  {
+    "revision": "a9a4d2a56c9cdd407418631ef6ba1649",
+    "url": "/supermath_ng_react/static/media/task_1.a9a4d2a5.jpg"
+  },
+  {
+    "revision": "cbfe1cd25c30fdb17f4bec6c459b3625",
+    "url": "/supermath_ng_react/static/media/task_10.cbfe1cd2.jpg"
+  },
+  {
+    "revision": "3a3eefb5bbddf6641773989fed0646ed",
+    "url": "/supermath_ng_react/static/media/task_12.3a3eefb5.jpg"
+  },
+  {
+    "revision": "1081cc687139eff30b21646065d7f50d",
+    "url": "/supermath_ng_react/static/media/task_13.1081cc68.jpg"
+  },
+  {
+    "revision": "f13df167634350f6eda81a316eb76862",
+    "url": "/supermath_ng_react/static/media/task_14.f13df167.jpg"
+  },
+  {
+    "revision": "f7dc0622c07e4feca0cac8517664b8e9",
+    "url": "/supermath_ng_react/static/media/task_2.f7dc0622.jpg"
+  },
+  {
+    "revision": "5eab40369cc1f27f3a9c1a8d659dabd6",
+    "url": "/supermath_ng_react/static/media/task_3.5eab4036.jpg"
+  },
+  {
+    "revision": "1935530dcbd521be54202b8f7f671142",
+    "url": "/supermath_ng_react/static/media/task_4.1935530d.jpg"
+  },
+  {
+    "revision": "9b3a550e5c37563f9746ad3a3b4efbec",
+    "url": "/supermath_ng_react/static/media/task_5.9b3a550e.jpg"
+  },
+  {
+    "revision": "5b8fea688003a4daf127da2ac216cecc",
+    "url": "/supermath_ng_react/static/media/task_6.5b8fea68.jpg"
+  },
+  {
+    "revision": "de89801caa68475cc2b9593c458de9f1",
+    "url": "/supermath_ng_react/static/media/task_7.de89801c.jpg"
+  },
+  {
+    "revision": "5e981920716433b76b61ea4fb120d085",
+    "url": "/supermath_ng_react/static/media/task_8.5e981920.jpg"
+  },
+  {
+    "revision": "553a46bc0f4daffa578568141853e3ef",
+    "url": "/supermath_ng_react/static/media/task_na.553a46bc.jpg"
+  },
+  {
+    "revision": "79bce60dda885b399a45f23a848db81c",
+    "url": "/supermath_ng_react/static/media/vitamins.79bce60d.jpg"
+  }
+]);
