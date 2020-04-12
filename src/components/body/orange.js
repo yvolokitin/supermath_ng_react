fe9d0bd3a1216b98d22eda5a2a05dd333ee411f1
Yura Volokitin <yuri.volokitin@gmail.com>
2020-04-12 16:37:26 +0200

    Update orange.js

--- src/components/body/orange.js
+++ src/components/body/orange.js
@@ -36,11 +36,11 @@ const games = [
     {id: 5, logo: logo5, type: '2digits', task: '+-,1-10,1-9,10,1', amount: task_amount},
     {id: 6, logo: logo6, type: '2digits', task: '+-,1-9,11-99,1,1', amount: task_amount},
 
-    {id: 7, logo: logo7, type: '2digits', task: '+-,11-99,11-99,1,1', amount: task_amount},
-    {id: 8, logo: logo8, type: '2digits', task: '+-,1-99,1-99,10,10', amount: task_amount},
+    {id: 7, logo: logo7, type: '2digits', task: '+-,1-9,11-99,10,1', amount: task_amount},
+    {id: 8, logo: logo8, type: '2digits', task: '+-,11-99,11-99,1,1', amount: task_amount},
     {id: 9, logo: logo9, type: '2digits', task: '+-,1-9,1-9,100,100', amount: task_amount},
 
-    {id: 10, logo: logo10, type: '2digits', task: '+-,1-9,11-99,10,1', amount: task_amount},
+    {id: 10, logo: logo10, type: '2digits', task: '+-,1-99,1-99,10,10', amount: task_amount},
     {id: 11, logo: logo11, type: '2digits', task: '+-,1-99,1-99,10,10', amount: task_amount},
     {id: 12, logo: logo12, type: 'digit_2column', task: '+-,11-99,101-999,1,1', amount: task_amount},
 ];
