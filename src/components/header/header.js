cf6dd5484662375a7df6ba4c5603ce071bd7d32a
Yura Volokitin <yuri.volokitin@gmail.com>
2020-04-16 22:03:17 +0200

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -34,7 +34,7 @@ export default class Header extends React.Component {
                       helpOpen: false,
                       loginOpen: false,
                       logoutOpen: false,
-                      forgetOpen: true, // false,
+                      forgetOpen: false,
                       registerOpen: false,
                       userInfoOpen: false,
                       langSelector: false,
