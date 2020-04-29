7d7a93085a51064b878f864d88f16f42e3dd65a2
Yura Volokitin <yuri.volokitin@gmail.com>
2020-04-30 00:05:47 +0200

    futher body tabs improvment

--- src/components/body/tabs.js
+++ src/components/body/tabs.js
@@ -10,24 +10,26 @@ export default function Tabs(props) {
     const [white, setWhite] = React.useState(0);
     const [orange, setOrange] = React.useState(0);
     const [green, setGreen] = React.useState(0);
-    const [navi, setNavi] = React.useState(0);
+    const [navy, setNavy] = React.useState(0);
     const [brown, setBrown] = React.useState(0);
     const [black, setBlack] = React.useState(0);
 
-    function unSet() {
+    function unset(color) {
         if (value === 'white') {
             setWhite('#fbfbf8');
         } else if (value === 'orange') {
             setOrange('#fbfbf8');
         } else if (value === 'green') {
             setGreen('#fbfbf8');
-        } else if (value === 'navi') {
-            setNavi('#fbfbf8');
+        } else if (value === 'navy') {
+            setNavy('#fbfbf8');
         } else if (value === 'brown') {
             setBrown('#fbfbf8');
         } else if (value === 'black') {
             setBlack('#fbfbf8');
         }
+
+        setValue(color);
     }
 
     function onClick(color) {
@@ -39,8 +41,15 @@ export default function Tabs(props) {
             setOrange('#3f51b5');
         } else if (color === 'green') {
             setGreen('#3f51b5');
+        } else if (color === 'navy') {
+            setNavy('#3f51b5');
+        } else if (color === 'brown') {
+            setBrown('#3f51b5');
+        } else if (color === 'black') {
+            setBlack('#3f51b5');
         }
 
+        unset(color);
     };
 
     return (
@@ -63,16 +72,22 @@ export default function Tabs(props) {
                 </div>
             </div>
 
-            <div className='tabs_div'>
-                <font style={{color:'#3f51b5'}}> {body[props.lang]['navy']} </font>
+            <div className='tabs_div_wrapper' style={{backgroundColor: navy}}>
+                <div className='tabs_div' onClick={() => onClick('navy')}>
+                    <font style={{color:'#3f51b5'}}> {body[props.lang]['navy']} </font>
+                </div>
             </div>
 
-            <div className='tabs_div'>
-                <font style={{color:'brown'}}> {body[props.lang]['brown']} </font>
+            <div className='tabs_div_wrapper' style={{backgroundColor: brown}}>
+                <div className='tabs_div' onClick={() => onClick('brown')}>
+                    <font style={{color:'brown'}}> {body[props.lang]['brown']} </font>
+                </div>
             </div>
 
-            <div className='tabs_div'>
-                <font style={{color:'black'}}> {body[props.lang]['black']} </font>
+            <div className='tabs_div_wrapper' style={{backgroundColor: black}}>
+                <div className='tabs_div' onClick={() => onClick('black')}>
+                    <font style={{color:'black'}}> {body[props.lang]['black']} </font>
+                </div>
             </div>
         </div>
     );
