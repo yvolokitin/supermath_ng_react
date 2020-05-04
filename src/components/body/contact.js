1b2581d0d8c94a4858d0311fe460d5869d266ec2
Yura Volokitin <yuri.volokitin@gmail.com>
2020-05-04 20:45:07 +0200

    improved footer and conatc components

---
+++ src/components/body/contact.js
@@ -0,0 +1,59 @@
+﻿import React from 'react';
+import {Dialog, TextField, Button} from '@material-ui/core';
+
+import CancelIcon from '@material-ui/icons/Cancel';
+
+import image from './../../images/contact/mail_icon.png';
+import ColorLine from './../line/line';
+import SMTitle from './../dialog/title';
+import './contact.css';
+
+export default function Info(props) {
+    return (
+        <Dialog open={props.open} transitionDuration={700} fullScreen={props.fullScreen} fullWidth={true} maxWidth='md' scroll='body'>
+            <SMTitle title='' onClick={() => props.onClose()}/>
+            <ColorLine/>
+
+            <div className='contact_email_wrapper'>
+                <div className='contact_email_image'>
+                    <div className='contact_email_image_img'>
+                        <img src={image} alt={image}/>
+                    </div>
+                    <div className='contact_email_image_alt_text'>
+                        If you have questions or just want to get in rouch, use the form below. We look forward to hearing from you!
+                    </div>
+                </div>
+
+                <div className='contact_email_form'>
+                    <div className='contact_info_title'> {props.title} </div>
+
+                    <div className='contact_textfield'>
+                        <TextField autoFocus required fullWidth variant='outlined' label='Your Name'/>
+                    </div>
+
+                    <div className='contact_textfield'>
+                        <TextField required fullWidth variant='outlined' label='Your Email Address'/>
+                    </div>
+
+                    <div className='contact_textfield'>
+                        <TextField required multiline fullWidth rows={4} variant='outlined' label='Message'/>
+                    </div>
+                </div>
+
+            </div>
+
+            <ColorLine/>
+
+            <div className='contact_connect_wrapper'>
+                or connect with us on...
+            </div>
+
+            <ColorLine/>
+
+            <div className='card_wrapper_btn' style={{height: '60px'}}>
+                <Button size='small' color='primary' startIcon={<CancelIcon/>}
+                        onClick={() => props.onClose()}> close </Button>
+            </div>
+        </Dialog>
+    );
+}
---
+++ src/components/body/footer.js
@@ -0,0 +1,50 @@
+﻿import React, { useState, useEffect } from 'react';
+import Link from '@material-ui/core/Link';
+
+import Contact from './contact';
+import {footer} from './../translations/footer';
+import './footer.css';
+
+export default function Footer(props) {
+    const [value, setValue] = useState(true); // useState(false);
+    const [fontColor, setFontColor] = useState('white');
+
+    useEffect(() => {
+        // console.log('Footer. useEffect ' + props.color);
+        if (props.color === 'white') {
+            setFontColor('#3f51b5');
+        } else if (props.color === 'yellow') {
+            setFontColor('brown');
+        } else if (props.color === 'green') {
+            setFontColor('orange');
+        } else if (props.color === 'orange') {
+            setFontColor('green');
+        } else if (props.color === 'black') {
+            setFontColor('white');
+        } else {
+            setFontColor('white');
+        }
+
+    }, [props.color, props.lang]);
+
+    /*
+        <footer style={{backgroundColor: props.color}}>
+        </footer>
+    */
+    return (
+        <div className='footer_wrapper' style={{backgroundColor: props.color, color: fontColor}}>
+            <div className='footer_copyright'>
+                {'Copyright © '} <Link color='inherit' href='http://supermath.xyz'>SuperMath.XYZ</Link>{' '} {new Date().getFullYear()}
+            </div>
+
+            <div className='footer_contacts'>
+                <font onClick={() => setValue(true)}>{footer[props.lang]['contacts']}</font>
+            </div>
+
+            <Contact open={value}
+                     title={footer[props.lang]['title']}
+                     text={footer[props.lang]['questions']}
+                     onClose={() => setValue(false)}/>
+        </div>
+    );
+}
