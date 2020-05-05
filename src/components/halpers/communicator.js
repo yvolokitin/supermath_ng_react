06aeaecd8de14750dfbddc1a38a606c31b881b11
Yura Volokitin <yuri.volokitin@gmail.com>
2020-05-06 00:16:22 +0200

    'communicator'

---
+++ src/components/halpers/communicator.js
@@ -0,0 +1,27 @@
+﻿import axios from 'axios';
+
+export function send_email(data) {
+    axios.post('http://supermath.xyz:3000/api/email', data)
+        .then(onApiUpdate)
+        .catch(onApiUpdateError);
+}
+
+function onApiCall(response) {
+    console.log('Communicator.onApiCall ' + response.toString());
+
+    if ('data' in response) {
+        if ('error' in response.data) {
+            console.log('ERROR: Communicator.onApiCall ' + response.data.error);
+
+        } else if ('id' in response.data) {
+            console.log('Communicator.onApiCall successed, id ' + response.data.id);
+
+        } else {
+            console.log('Communicator.onApiCall received no data in response from server');
+        }
+    }
+
+function onApiCallError(error) {
+    console.log('Communicator.onApiCallError -> error ' + error);
+
+}
