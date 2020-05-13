a26acd333e2d0e02711920a03164c60d0c8f907f
Yura Volokitin <yuri.volokitin@gmail.com>
2020-05-14 00:17:37 +0200

    added

--- src/components/header/trophy.js
+++ src/components/header/trophy.js
@@ -36,7 +36,7 @@ export default function Trophy(props) {
     }, [ ])
 
     const onFailed = useCallback((response) => {
-        console.log('Header.onFailed received ' + response.data[1].name);
+        // console.log('Header.onFailed received ' + response.data[1].name);
         var array = [];
         for (var i = 1; i < 11; i++) {
             // console.log('response.data[i] ' + response.data[i].name + ': ' + response.data[i].pass);
@@ -65,7 +65,7 @@ export default function Trophy(props) {
     }, [onFailed, onError])
 
     useEffect(() => {
-        console.log('Trophy.props.open ' + props.open);
+        // console.log('Trophy.props.open ' + props.open);
         if (props.open === true) {
             setPassing(true);
             setFailing(true);
@@ -76,7 +76,41 @@ export default function Trophy(props) {
     }, [props.open, props.lang, getPassed, getFailed]);
 
     /*
-
+                <div className='trophy_table_wrapper' style={{backgroundColor:'brown',color:'white',}}>
+                    { (failing === true) ? (
+                        <img src={image} alt='progress'/>
+                      ) : (
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align='center' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold'}}>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127942;</span>
+                                    </TableCell>
+                                    <TableCell align='left' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold'}}>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128102;</span>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127947;</span>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128105;</span>
+                                    </TableCell>
+                                    <TableCell align='center' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold'}}>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127774;</span>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127919;</span>
+                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128142;</span>
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {failed.map((user, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell align='center' component='th' scope='row' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold',color:'white',}}> {index+1} </TableCell>
+                                        <TableCell align='left' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold',color:'white',}}> {user.name} {user.surname} </TableCell>
+                                        <TableCell align='center' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold',color:'white',}}> {user.fail} </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                      )
+                    }
+                </div>
     */
     return (
         <Dialog open={props.open} onClose={() => props.onClose()}
@@ -91,6 +125,40 @@ export default function Trophy(props) {
             </div>
 
             <div className='trophy_wrapper'>
+                { (passing === true) ? (
+                    <img src={image} alt='progress'/>
+                ) : (
+                    <div className='trophy_table_wrapper'>
+                        <div className='trophy_table_row'>
+                            <div className='trophy_table_cell_num'>
+                                <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127942;</span>
+                            </div>
+                            <div className='trophy_table_cell_name'>
+                                <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128102;</span>
+                                <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127947;</span>
+                                <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128105;</span>
+                            </div>
+                            <div className='trophy_table_cell_res'>
+                                <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127919;</span>
+                            </div>
+                        </div>
+                        {passed.map((user, index) => (
+                            <div className='trophy_table_row' key={index}>
+                                <div className='trophy_table_cell_num'>
+                                    {index+1}
+                                </div>
+                                <div className='trophy_table_cell_name'>
+                                    {user.name} {user.surname}
+                                </div>
+                                <div className='trophy_table_cell_res'>
+                                    {user.pass}
+                                </div>
+                            </div>
+                        ))}
+
+                    </div>
+                )}
+
                 <div className='trophy_table_wrapper'>
                     { (passing === true) ? (
                         <img src={image} alt='progress'/>
@@ -98,15 +166,15 @@ export default function Trophy(props) {
                         <Table>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>
+                                    <TableCell align='center' style={{fontSize:'1.4rem',lineHeight:'0.9',fontWeight: 'bold'}}>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127942;</span>
                                     </TableCell>
-                                    <TableCell align='left'>
+                                    <TableCell align='left' style={{fontSize:'1.4rem',lineHeight:'0.9',fontWeight: 'bold'}}>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128102;</span>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127947;</span>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128105;</span>
                                     </TableCell>
-                                    <TableCell align='center'>
+                                    <TableCell align='center' style={{fontSize:'1.4rem',lineHeight:'0.9',fontWeight: 'bold'}}>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127774;</span>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127919;</span>
                                         <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128142;</span>
@@ -116,45 +184,9 @@ export default function Trophy(props) {
                             <TableBody>
                                 {passed.map((user, index) => (
                                     <TableRow key={index}>
-                                        <TableCell component='th' scope='row'> {index+1} </TableCell>
-                                        <TableCell align='left'> {user.name} {user.surname} </TableCell>
-                                        <TableCell align='center'> {user.pass} </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                      )
-                    }
-                </div>
-
-                <div className='trophy_table_wrapper'>
-                    { (failing === true) ? (
-                        <img src={image} alt='progress'/>
-                      ) : (
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127942;</span>
-                                    </TableCell>
-                                    <TableCell align='left'>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128102;</span>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127947;</span>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128105;</span>
-                                    </TableCell>
-                                    <TableCell align='center'>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127774;</span>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#127919;</span>
-                                        <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128142;</span>
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {failed.map((user, index) => (
-                                    <TableRow key={index}>
-                                        <TableCell component='th' scope='row'> {user.id} </TableCell>
-                                        <TableCell align='left'> {user.name} {user.surname} </TableCell>
-                                        <TableCell align='center'> {user.fail} </TableCell>
+                                        <TableCell align='center' component='th' scope='row' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold'}}> {index+1} </TableCell>
+                                        <TableCell align='left' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold',overflow:'hidden',textOverflow:'clip'}}> {user.name} {user.surname} </TableCell>
+                                        <TableCell align='center' style={{fontSize:'1.2rem',lineHeight:'0.9',fontWeight: 'bold'}}> {user.pass} </TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
