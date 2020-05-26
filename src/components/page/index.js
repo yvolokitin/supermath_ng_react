ec1de891f9a013024cad3663994d269d2af65675
Yura Volokitin <yuri.volokitin@gmail.com>
2020-05-26 13:38:01 +0200

    added facebook

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -67,6 +67,20 @@ export default class SuperMathPage extends React.Component {
 
         this.onWidthChange = this.onWidthChange.bind(this);
 
+        window.fbAsyncInit = function() {
+        FB.init({appId:'245721866654291',cookie:true,xfbml:true,version:'v7.0'});
+        FB.AppEvents.logPageView();};
+        (function(d, s, id) {
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+        } (document, 'script', 'facebook-jssdk'));
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
         this.state = {
             width: window.innerWidth,
             screen: STATUS.NONE,
