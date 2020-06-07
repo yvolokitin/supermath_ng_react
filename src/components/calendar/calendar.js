e94bff64bf3ce93e090d7d3b449566228813e44b
Yura Volokitin <yuri.volokitin@gmail.com>
2020-06-08 01:04:13 +0200

    progress correction

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -8,62 +8,16 @@ const monthes = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
 const weekdays = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
 
 export default function Calendar(props) {
-    const [results, setResults] = useState([]);
-    const [current, setCurrentMonth] = useState(0);
+    const [results, set_results] = useState([]);
+    const [month, set_month] = useState(0);
     const [year, set_year] = useState(0);
-    const [days, setDays] = useState([]);
-
-    /**
-     * Set number of days per exact month-year
-     * have to increment month due to Date().getMonth() returns month in range 0-11
-     */
-    const set_month_days = useCallback((month, year) => {
-        // console.log('set_month_days, month ' + month + ', year ' + year);
-        var days_num = [], number = new Date(year, month + 1, 0).getDate();
-
-        // filling array with empty 
-        var counter = 0;        
-        while (counter < (weekdays.length - 1)) {
-            var first = (new Date(year, month, 1)).getDay();
-            if (weekdays[first] !== weekdays[counter + 1]) {
-                days_num[counter] = {
-                    'id': 'fake_day_' + (counter+1),
-                    'date': '',
-                    'day': '',
-                    'name': '',
-                };
-                counter++;
-            } else {
-                break;
-            }
-        }
-
-        for (var i = 0; i < number; i++) {
-            var date = new Date(year, month, i+1);
-            // getDay() is an integer corresponding to the day of the week:
-            // 0 for Sunday, 1 for Monday, 2 for Tuesday, and so on.
-            var day_name = weekdays[date.getDay()];
-            var name = calendar[props.lang][day_name];
-            var day_id = date.toString() + ', ' + i;
-
-            days_num[(i+counter)] = {
-                'id': day_id,
-                'date': date,
-                'day': (i+1),
-                'name': name,
-            };
-            // console.log('day_num ' + i + ', name ' + name);
-        }
-
-        setDays(days_num);
-        setCurrentMonth(month);
-    }, [props.lang] )
+    const [days, set_days] = useState([]);
 
     const onResultsUpdate = useCallback((response) => {
         var array = [];
         for (var name in response.data) {
             var date = new Date(response.data[name]['date']);
-            console.log('Calendar.onResultsUpdate received ' + date.getDate() + ': ' + response.data[name]['percent']);
+            // console.log('Calendar.onResultsUpdate received ' + date.getDate() + ': ' + response.data[name]['percent']);
 
             var data = {
                 'date': date,
@@ -79,79 +33,127 @@ export default function Calendar(props) {
             array.push(data);
         }
 
-        setResults(array);
+        set_results(array);
     }, [ ])
 
     const onResultsError = useCallback((error) => {
         console.log('Header.onScoresError ' + error);
     }, [ ])
 
-    /*
-         = request.json.get('user_id')
-        pswdhash = request.json.get('pswdhash')
-        month = request.json.get('month')
-    */
-    const get_results = useCallback(() => {
-        var post_data = {
-            'user_id': props.id,
-            'pswdhash': props.pswdhash,
-            'month': current,
-        };
-        axios.post('http://supermath.xyz:3000/api/results', post_data)
-             .then(onResultsUpdate)
-             .catch(onResultsError);
+    const get_results = useCallback((new_month, new_year) => {
+        if (new_month !== month) {
+            console.log('!!!!!!!!!! get_results');
+            var post_data = {
+                'user_id': props.id,
+                'pswdhash': props.pswdhash,
+                'month': new_month,
+                'year': new_year,
+            };
+            axios.post('http://supermath.xyz:3000/api/results', post_data)
+                 .then(onResultsUpdate)
+                 .catch(onResultsError);
+        }
 
-    }, [current, props.id, props.pswdhash, onResultsUpdate, onResultsError])
+    }, [props.id, props.pswdhash, month, onResultsUpdate, onResultsError])
+
+    /**
+     * Set number of days per exact month-year
+     * have to increment month due to Date().getMonth() returns month in range 0-11
+     */
+    const set_month_days = useCallback((new_month, new_year) => {
+        console.log('set_month_days, new_month ' + new_month + ', old_month ' + month + ', new_year ' + new_year);
+        if (new_month !== month) {
+            var days_num = [], number = new Date(new_year, new_month+1, 0).getDate();
+
+            // filling array with empty 
+            var counter = 0;        
+            while (counter < (weekdays.length - 1)) {
+                var first = (new Date(new_year, new_month, 1)).getDay();
+                if (weekdays[first] !== weekdays[counter + 1]) {
+                    days_num[counter] = {
+                        'id': 'fake_day_' + (counter+1),
+                        'date': '',
+                        'day': '',
+                        'name': '',
+                    };
+                    counter++;
+                } else {
+                    break;
+                }
+            }
+
+            for (var i = 0; i < number; i++) {
+                var date = new Date(new_year, new_month, i+1);
+                // getDay() is an integer corresponding to the day of the week:
+                // 0 for Sunday, 1 for Monday, 2 for Tuesday, and so on.
+                var day_name = weekdays[date.getDay()];
+                var name = calendar[props.lang][day_name];
+                var day_id = date.toString() + ', ' + i;
+                days_num[(i+counter)] = {
+                    'id': day_id,
+                    'date': date,
+                    'day': (i+1),
+                    'name': name,
+                };
+            }
+
+            set_days(days_num);
+        }
+
+    }, [props.lang, month] )
 
     /**
      * useEffect is Effect Hook for componentDidMount and componentDidUpdate
      */
     useEffect(() => {
-        var current_year = (new Date()).getFullYear();
         var current_month = (new Date()).getMonth();
+        var current_year = (new Date()).getFullYear();
 
-        set_month_days(current_month, current_year);
+        console.log('!!!!!!!!!! useEffect -> month ' + current_month + ', year ' + current_year);
+
+        set_month(current_month);
         set_year(current_year);
-
-        setResults([]);
+        set_month_days(current_month, current_year);
 
         if (props.id > 0) {
-            get_results();
+            get_results(current_month, current_year);
+        } else {
+            set_results([]);
         }
 
     }, [props.lang, props.id, set_month_days, get_results]);
 
-    function onMonthChange(navigation) {
+    function on_month(navigation) {
         if (navigation > 0) { // next month
-            if (current > (monthes.length-2)) {
-                set_month_days(0, year + 1);
+            if (month > (monthes.length-2)) {
+                set_month(0);
                 set_year(year + 1);
+
             } else {
-                set_month_days(current + 1, year);
+                set_month(month + 1);
             }
         } else { // previous month
-            if (current > 0) {
-                set_month_days(current - 1, year);
+            if (month > 0) {
+                set_month(month - 1);
             } else {
-                set_month_days(11, year - 1);
+                set_month(11);
                 set_year(year - 1);
             }
         }
     }
 
     /*
-results.length > 0
     */
     return (
         <>
             <div className='calendar_wrapper'>
-                <div className='calendar_prev_next' style={{float: 'left'}} onClick={() => onMonthChange(-1)}>&#10094;</div>
+                <div className='calendar_prev_next' style={{float: 'left'}} onClick={() => on_month(-1)}>&#10094;</div>
 
                 <div className='calendar_month' style={{float: 'left'}}>
-                    {calendar[props.lang][monthes[current]]} <br/> {year}
+                    {calendar[props.lang][monthes[month]]} <br/> {year}
                 </div>
                 
-                <div className='calendar_prev_next' style={{float: 'right'}} onClick={() => onMonthChange(1)}>&#10095;</div>
+                <div className='calendar_prev_next' style={{float: 'right'}} onClick={() => on_month(1)}>&#10095;</div>
             </div>
 
             <div className='calendar_weekdays'>
@@ -167,13 +169,21 @@ results.length > 0
             <div className='calendar_days'>
                 {
                     days.map((day, index) => (
-                        <li key={day.id}>
+                        <div className='calendar_days_cell' key={day.id}>
                             {(results.some(result => result.day === day.day)) ? (
-                                <font style={{color:'green'}}>{day.day}</font>
+                                <div className='calendar_days_cell_does'>
+                                    <div className='calendar_days_cell_circle'>
+                                        {day.day}
+                                    </div>
+                                </div>
                             ) : (
-                                <font style={{color:'red'}}>{day.day}</font>
+                                <div className='calendar_days_cell_does'>
+                                    <div className='calendar_days_cell_empty'>
+                                        {day.day}
+                                    </div>
+                                </div>
                             )}
-                        </li>
+                        </div>
                     ))
                 }
             </div>
