fb83749ff380d989d566bcbbe7eb0fc242a3a2f6
Yura Volokitin <yuri.volokitin@gmail.com>
2020-07-31 16:55:03 +0200

    latest

--- src/components/games/gameexit.js
+++ src/components/games/gameexit.js
@@ -2,17 +2,15 @@
 import {Button, Slide, Typography} from '@material-ui/core';
 import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core';
 
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 
-import RestoreIcon from '@material-ui/icons/Restore';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SettingsIcon from '@material-ui/icons/Settings';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 import SMTitle from './../dialog/title';
 import ColorLine from './../line/line';
@@ -26,7 +24,10 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function GameExit(props) {
     return (
-        <Dialog open={props.open} onClose={() => props.onClose('close')} fullScreen={props.fullScreen} TransitionComponent={Transition} transitionDuration={900}>
+        <Dialog open={props.open} onClose={() => props.onClose('close')}
+                scroll='body' fullScreen={props.fullScreen}
+                TransitionComponent={Transition} transitionDuration={900}>
+
             <SMTitle title='' onClick={() => props.onClose('close')}/>
             <ColorLine/>
 
@@ -45,12 +46,11 @@ export default function GameExit(props) {
             <DialogContent>
             </DialogContent>
 
-            <DialogContent>
+            <DialogContent scroll='body'>
                 <BottomNavigation showLabels>
-                    <BottomNavigationAction label='Recents' value='recents' icon={<RestoreIcon/>}/>
-                    <BottomNavigationAction label='Favorites' value='favorites' icon={<FavoriteIcon/>}/>
+                    <BottomNavigationAction label='Settings' value='settings' icon={<SettingsIcon/>}/>
                     <BottomNavigationAction label='Help' value='favorites' icon={<HelpOutlineIcon/>}/>
-                    <BottomNavigationAction label='Previous Task' value='back' icon={<ArrowBackIcon/>}/>
+                    <BottomNavigationAction disabled label='Previous' value='back' icon={<ArrowBackIcon/>}/>
                     <BottomNavigationAction label='Next Task' value='next' icon={<ArrowForwardIcon/>}/>
                 </BottomNavigation>
             </DialogContent>
