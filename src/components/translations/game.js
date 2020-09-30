bda759072f895558e41f982dd325138af29d4448
Yura Volokitin <yuri.volokitin@gmail.com>
2020-09-30 21:59:06 +0200

    latest

--- src/components/translations/game.js
+++ src/components/translations/game.js
@@ -21,7 +21,8 @@ export const game = {
 
         'info': 'INFO',
         'info_title': 'Info',
-        'info_question': '',
+        'info_question': 'Use SuperMath as mathematical vitamins!',
+        'info_text': 'SuperMath is designed to help students transition from counting or calculating to recalling the basic arithmetic facts.',
 
         'results': 'RESULTS',
         'results_title': 'Results',
@@ -55,6 +56,8 @@ export const game = {
 
         'info': 'О ИГРЕ',
         'info_title': 'О игре',
+        'info_question': 'SuperMath - это ежедневная витаминка для ваших детей!',
+        'info_text': 'SuperMath разработан для того, чтобы помочь детям и взрослым научиться делать быстрые математические действия в уме.',
 
         'results': 'РЕЗУЛЬТАТЫ',
         'results_title': 'Результаты',
@@ -69,62 +72,177 @@ export const game = {
     },
     // Dutch
     'nl' : {
-        'exit': 'EXIT',
+        'exit': 'UITGANG',
+        'exit_title': 'Uitgang',
+        'exit_question': 'Wil je echt het spel verlaten?',
+        'exit_text': 'U verliest al uw geslaagde scores-gegevens als u afsluit (de teller voor mislukken wordt onder uw profiel bewaard)! We raden u ten zeerste aan om de huidige wiskundige taken te voltooien.',
+
         'settings': 'INSTELLINGEN',
-        'results': 'Resultaten',
+        'settings_title': 'Instellingen',
+        'settings_question': 'U kunt hier de visuele parameters wijzigen',
+        'settings_text': 'De interface in de donkere modus is een licht-op-donker kleurenschema, ook wel de donkere modus genoemd, die lichtgekleurde tekst, pictogrammen en grafische gebruikersinterface-elementen op een donkere achtergrond gebruikt.',
+        'settings_dark': 'Schakel de donkere modus-interface in',
+        'settings_keyboard': 'Toon toetsenbord vanaf de rechterkant',
+
+        'help': 'HELPEN',
+        'help_title': 'Helpen',
+        'help_question': 'Hoe kunnen we u helpen?',
+        'help_text': 'Als u vragen heeft of hulp nodig heeft, kunt u het contactformulier onderaan de hoofdpagina gebruiken',
+
+        'info': 'INFO',
+        'info_title': 'Info',
+        'info_question': 'Use SuperMath as mathematical vitamins!',
+        'info_text': 'SuperMath is designed to help students transition from counting or calculating to recalling the basic arithmetic facts.',
+
+        'results': 'RESULTATEN',
+        'results_title': 'Resultaten',
+
         'previous': 'Vorige',
         'next': 'Volgende Taak',
-        'help': 'Helpen',
-        'close': 'dichtbij',
 
-        'settings_title': 'You can change the visual parameters here',
+        'answer_yes': 'Uitgang',
+        'answer_no': 'Laat me blijven en doorgaan',
+
+        'close': 'dichtbij',
     },
     // French
     'fr' : {
-        'exit': 'EXIT',
+        'exit': 'SORTIE',
+        'exit_title': 'Sortie',
+        'exit_question': 'Voulez-vous vraiment quitter le jeu?',
+        'exit_text': 'Vous perdrez toutes vos données de scores réussis si vous quittez (lorsque le compteur Échec sera conservé sous votre profil)! Nous vous recommandons vivement de terminer les tâches mathématiques en cours.',
+
         'settings': 'PARAMETRES',
-        'results': 'Résultats',
+        'settings_title': 'Parameters',
+        'settings_question': 'Vous pouvez modifier les paramètres visuels ici',
+        'settings_text': 'L\'interface en mode sombre est un jeu de couleurs clair sur foncé, également appelé mode sombre qui utilise du texte, des icônes et des éléments d\'interface utilisateur graphique de couleur claire sur un fond sombre.',
+        'settings_dark': 'Activer l\'interface du mode sombre',
+        'settings_keyboard': 'Afficher le clavier du côté droit',
+
+        'help': 'D\'AIDE',
+        'help_title': 'D\'aide',
+        'help_question': 'Comment pouvons-nous vous aider?',
+        'help_text': 'N\'hésitez pas à utiliser le formulaire Contacts en bas de la page principale, si vous avez des questions ou avez besoin d\'aide',
+
+        'info': 'INFO',
+        'info_title': 'Info',
+        'info_question': 'Use SuperMath as mathematical vitamins!',
+        'info_text': 'SuperMath is designed to help students transition from counting or calculating to recalling the basic arithmetic facts.',
+
+        'results': 'RÉSULTATS',
+        'results_title': 'Résultats',
+
         'previous': 'Précédent',
         'next': 'Tâche Suivante',
-        'help': 'Aidez-moi',
-        'close': 'proche',
 
-        'settings_title': 'You can change the visual parameters here',
+        'answer_yes': 'Sortie',
+        'answer_no': 'Laisse-moi rester et continuer',
+
+        'close': 'proche',
     },
     // German
     'de' : {
-        'exit': 'EXIT',
+        'exit': 'AUSGANG',
+        'exit_title': 'Ausgang',
+        'exit_question': 'Möchten Sie das Spiel wirklich beenden?',
+        'exit_text': 'Sie verlieren alle Ihre bestandenen Punktedaten, wenn Sie beenden (wenn der Fehlerzähler unter Ihrem Profil bleibt)! Wir empfehlen Ihnen dringend, die aktuellen mathematischen Aufgaben zu erledigen.',
+
         'settings': 'EINSTELLUNGEN',
-        'results': 'Ergebnisse',
+        'settings_title': 'Einstellungen',
+        'settings_question': 'Hier können Sie die visuellen Parameter ändern',
+        'settings_text': 'Die Dunkelmodus-Oberfläche ist ein Hell-Dunkel-Farbschema, das auch als Dunkelmodus bezeichnet wird und hellen Text, Symbole und grafische Elemente der Benutzeroberfläche auf dunklem Hintergrund verwendet.',
+        'settings_dark': 'Aktivieren Sie die Dark-Mode-Schnittstelle',
+        'settings_keyboard': 'Tastatur von rechts anzeigen',
+
+        'help': 'HILFE',
+        'help_title': 'Hilfe',
+        'help_question': 'Wie können wir Ihnen helfen?',
+        'help_text': 'Wenn Sie Fragen haben oder Hilfe benötigen, können Sie das Kontaktformular unten auf der Hauptseite verwenden.',
+
+        'info': 'INFO',
+        'info_title': 'Info',
+        'info_question': 'Use SuperMath as mathematical vitamins!',
+        'info_text': 'SuperMath is designed to help students transition from counting or calculating to recalling the basic arithmetic facts.',
+
+        'results': 'ERGEBNISSE',
+        'results_title': 'Ergebnisse',
+
         'previous': 'Bisherige',
         'next': 'Nächste Aufgabe',
-        'help': 'Hilfe',
-        'close': 'nah',
 
-        'settings_title': 'You can change the visual parameters here',
+        'answer_yes': 'Ausgang',
+        'answer_no': 'Lass mich bleiben und weitermachen',
+
+        'close': 'nah',
     },
     // Spanish
     'es' : {
-        'exit': 'EXIT',
+        'exit': 'SALIDA',
+        'exit_title': 'Salida',
+        'exit_question': 'De verdad quieres salir del juego?',
+        'exit_text': 'Perderá todos los datos de sus puntuaciones aprobadas si sale (cuando el contador de fallos se mantendrá en su perfil)! Le recomendamos encarecidamente que termine las tareas actuales de matemáticas.',
+
         'settings': 'CONFIGURACIONES',
-        'results': 'Resultados',
+        'settings_title': 'Configuraciones',
+        'settings_question': 'Puede cambiar los parámetros visuales aquí',
+        'settings_text': 'La interfaz del modo oscuro es un esquema de color claro sobre oscuro, también llamado modo oscuro que utiliza texto, iconos y elementos de la interfaz gráfica de usuario de colores claros sobre un fondo oscuro.',
+        'settings_dark': 'Habilitar la interfaz del modo oscuro',
+        'settings_keyboard': 'Mostrar teclado desde el lado derecho',
+
+        'help': 'AYUDA',
+        'help_title': 'Ayuda',
+        'help_question': 'Como podemos ayudarte?',
+        'help_text': 'Por favor, siéntase libre de usar el formulario Contactos en la parte inferior de la página principal, si tiene alguna pregunta o necesita ayuda.',
+
+        'info': 'INFO',
+        'info_title': 'Info',
+        'info_question': '¡Usa SuperMath como vitaminas matemáticas!',
+        'info_text': 'SuperMath está diseñado para ayudar a los estudiantes a pasar de contar o calcular a recordar las operaciones aritméticas básicas.',
+
+        'results': 'RESULTADOS',
+        'results_title': 'Resultados',
+
         'previous': 'Previo',
         'next': 'Siguiente Tarea',
-        'help': 'Ayuda',
-        'close': 'cerca',
 
-        'settings_title': 'You can change the visual parameters here',
+        'answer_yes': 'Salida',
+        'answer_no': 'Déjame quedarme y continuar',
+
+        'close': 'cerca',
     },
     // Italian
     'it' : {
-        'exit': 'EXIT',
+        'exit': 'USCITA',
+        'exit_title': 'Uscita',
+        'exit_question': 'Vuoi davvero uscire dal gioco?',
+        'exit_text': 'Se esci perderai tutti i dati relativi ai punteggi passati (quando il contatore dei fallimenti sarà mantenuto sotto il tuo profilo)! Ti consigliamo vivamente di completare le attività matematiche correnti.',
+
         'settings': 'IMPOSTAZIONI',
-        'results': 'Risultati',
+        'settings_title': 'Impostazioni',
+        'settings_question': 'Puoi modificare i parametri visivi qui',
+        'settings_text': 'L\'interfaccia della modalità scura è una combinazione di colori chiaro su scuro, chiamata anche modalità scura che utilizza testo, icone ed elementi grafici dell\'interfaccia utente di colore chiaro su uno sfondo scuro.',
+        'settings_dark': 'Abilita l\'interfaccia in modalità oscura',
+        'settings_keyboard': 'Mostra la tastiera dal lato destro',
+
+        'help': 'AIUTO',
+        'help_title': 'Aiuto',
+        'help_question': 'Come possiamo aiutarti?',
+        'help_text': 'Per favore, sentiti libero di usare il modulo Contatti in fondo alla pagina principale, se hai domande o hai bisogno di aiuto',
+
+        'info': 'INFO',
+        'info_title': 'Info',
+        'info_question': 'Usa SuperMath come vitamine matematiche!',
+        'info_text': 'SuperMath è progettato per aiutare gli studenti a passare dal conteggio o calcolo al richiamo dei fatti aritmetici di base.',
+
+        'results': 'RISULTATI',
+        'results_title': 'Risultati',
+
         'previous': 'Precedente',
         'next': 'Attività successiva',
-        'help': 'Aiuto',
-        'close': 'close',
 
-        'settings_title': 'You can change the visual parameters here',
+        'answer_yes': 'Uscita',
+        'answer_no': 'Lasciami restare e continua',
+
+        'close': 'close',
     },
 };
