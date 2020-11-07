2ea944aecfd69fcf40db03aab5096cc0862dda7e
Yura Volokitin <yuri.volokitin@gmail.com>
2020-11-08 00:03:05 +0100

    fixed in progress

--- src/components/games/taskgame.js
+++ src/components/games/taskgame.js
@@ -28,27 +28,25 @@ export default function TaskGame(props) {
     const [task, setTask] = useState('');
     const [loading, setLoading] = useState(true);
 
+    const [result, setResult] = useState('?');
+    const [image, setImage] = useState('');
+
     const [counter, setCounter] = useState(0);
-    const [total, setTotal] = useState(0);
     const [passed, setPassed] = useState(0);
     const [failed, setFailed] = useState(0);
-
-    const [image, setImage] = useState('');
-    const [answer, setAnswer] = useState('?');
-    const [result, setResult] = useState('?');
 
     const [float_desk, setFloatDesk] = useState('left');
     const [float_board, setFloatBoard] = useState('right');
 
-    const [font, setFont] = useState('grey');
     const [animation, setAnimation] = useState('blinker 5s linear infinite');
+    const [font, setFont] = useState('grey');
 
     const [openAlert, setOpenAlert] = useState(ALERT.NONE);
 
-    const onGetNewTaskUpdate = useCallback((response) => {
+    const onUpdate = useCallback((response) => {
         if ('data' in response) {
             if ('error' in response.data) {
-                console.log('onGetNewTaskUpdate error ' + response.data.error);
+                console.log('TaskGame.onUpdate -> ' + response.data.error);
                 setTask('Error: ' + response.data.error);
                 setImage(url_prefix + 'sm_error.jpg');
 
@@ -57,48 +55,40 @@ export default function TaskGame(props) {
                 setTask(response.data.description);
                 setResult(response.data.result);
                 setImage(url_prefix + response.data.image);
-
-                // set answer to ? and blinking
-                setCounter(0); setFont('grey'); setAnswer('?');
-                setAnimation('blinker 5s linear infinite');
+                setFont('grey'); setAnimation('blinker 5s linear infinite');
             }
         }
 
+        setCounter(prevCounter => prevCounter + 1);
         setLoading(false);
-    }, [ ])
+    }, [ ]);
 
-    const onGetNewTaskError = useCallback((error) => {
-        console.log('Header.onScoresError ' + error);
+    const onError = useCallback((error) => {
+        console.log('TaskGame.onError ' + error);
         setTask('Error: ' + error);
         setLoading(false);
-    }, [ ])
-
-    const getNewTask = useCallback(() => {
-        var data = {'lang': props.lang, 'level': props.task.uid}
-        axios.post('https://supermath.xyz:3000/api/gettask', data)
-             .then(onGetNewTaskUpdate)
-             .catch(onGetNewTaskError);
-
-    }, [props.lang, props.task, onGetNewTaskUpdate, onGetNewTaskError])
+    }, [ ]);
 
     useEffect(() => {
         if (props.open) {
             console.log('TaskGame.useEffect -> ' + props.task.uid);
 
-            setFloatDesk('left'); setFloatBoard('right');
-            setLoading(true); getNewTask();
+            if (counter === 0) {
+                setFloatDesk('left'); setFloatBoard('right');
+                setPassed(0); setFailed(0); setFont('grey');
+                setAnimation('blinker 5s linear infinite');
+            }
 
-        // } else {
-            setCounter(0); setTotal(0);
-            setPassed(0); setFailed(0);
-            setAnswer('?'); setResult('?');
-            setFont('grey'); setAnimation('blinker 5s linear infinite');
+            setLoading(true);
+            var data = {'lang': props.lang, 'level': props.task.uid}
+            axios.post('https://supermath.xyz:3000/api/gettask', data)
+                .then(onUpdate).catch(onError);
         }
 
-    }, [props.open, props.task, getNewTask]);
+    }, [props.open, props.task, props.lang, counter, onUpdate, onError, ]);
 
     const onDialog = (status) => {
-        console.log('TaskGame.onDialog ' + status);
+        console.log('TaskGame.onDialog ' + status + ', loading ' + loading);
         if (loading === false) {
             if (status === 'close') {
                 setOpenAlert(ALERT.NONE);
@@ -118,7 +108,7 @@ export default function TaskGame(props) {
 
             } else if (status === 'next') {
                 // console.log('Proceed with Next Task');
-                setLoading(true); getNewTask();
+                // setLoading(true); getNewTask();
                 setOpenAlert(ALERT.NONE);
 
             } else { // close
@@ -127,61 +117,15 @@ export default function TaskGame(props) {
         }
     }
 
-    const onDigit = (number) => {
-        // console.log('TaskGame.onDigit ' + number);
-        // if result is ? -> some error from server and we have no task
-        if (result !== '?') {
-            if (answer === '?') {
-                setAnswer(number); setFont('black');
-                setAnimation('none');
-            } else {
-                var new_aswer = answer + number;
-                setAnswer(new_aswer);
-            }
-        }
+    function onAnswer(answer) {
+        console.log('TaskGame.onAnswer -> answer ' + answer + ', expected ' + result);
     }
 
-    const onOperator = (symbol) => {
-        // console.log('TaskGame.onOperator ' + symbol);
-        if (symbol === 'clear' && answer !== '?' && result !== '?') {
-            if (answer.length === 1) {
-                setFont('grey'); setAnswer('?');
-                setAnimation('blinker 5s linear infinite');
-            } else {
-                setAnswer(answer.slice(0, -1));
-            }
-        } else if (symbol === 'enter') {
-            checkAnswer();
-        }
-    }
-
-    function checkAnswer() {
-        console.log('answer ' + answer + ', result ' + result);
-        if (answer !== '?') {
-            if (answer === result) {
-                setTotal(total => total + 1);
-
-                if (counter === 0) {
-                    setPassed(passed => passed + 1);
-                }
-
-                setFont('green');
-                setLoading(true);
-
-                setTimeout(() => {
-                    getNewTask();
-                }, 1300);
-
-            } else {
-                if (counter === 0) {
-                    setFailed(failed => failed + 1);
-                }
-
-                setCounter(counter => counter + 1);
-            }
-        }
-    }
-
+    /*
+                    <div className='taskgame_gameboard_answer_wrapper'>
+                        <font style={{'color': font, 'animation': animation}}> {answer} </font>
+                    </div>
+    */
     return (
         <Dialog open={props.open} fullScreen={true} TransitionComponent={Transition} transitionDuration={900}>
             <div className='taskgame_header_wrapper'>
@@ -190,7 +134,7 @@ export default function TaskGame(props) {
                 </div>
                 <div className='taskgame_header_wrapper_right' style={{'float': float_board}}>
                     <font style={{color: 'black'}}>
-                        {total} <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128279;</span>
+                        {counter} <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128279;</span>
                     </font>
                     <font style={{color: 'green'}}>
                         {passed} <span role='img' aria-labelledby='jsx-a11y/accessible-emoji'>&#128515;</span>
@@ -207,15 +151,13 @@ export default function TaskGame(props) {
                         <img src={image} alt='task' onContextMenu={(e) => e.preventDefault()}/>
                     </div>
 
-                    <div className='taskgame_gameboard_answer_wrapper'>
-                        <font style={{'color': font, 'animation': animation}}> {answer} </font>
-                    </div>
-
-                    <div className='taskgame_gameboard_wrapper'> {task} </div>
+                    <div className='taskgame_gameboard_task_wrapper'> {task} </div>
                 </div>
 
                 <div className='taskgame_body_wrapper_right' style={{'float': float_board}}>
-                    <EnterKeyboard navi='true' onDigit={onDigit} onOperator={onOperator}/>
+                    <EnterKeyboard open={loading}
+                        animation={animation}
+                        onAnswer={onAnswer}/>
                 </div>
             </div>
 
--- src/components/keyboard/enterkeyboard.js
+++ src/components/keyboard/enterkeyboard.js
@@ -4,23 +4,66 @@ import './enterkeyboard.css';
 /*
 */
 export default function EnterKeyboard(props) {
+    const [animation, setAnimation] = React.useState('blinker 5s linear infinite');
+    const [font, setFont] = React.useState('grey');
+    const [result, setResult] = React.useState('?');
+
+    const onDigit = (number) => {
+        console.log('EnterKeyboard.onDigit ' + number);
+        // if result is ? -> some error from server and we have no task
+        if (result === '?') {
+            setResult(number); setFont('black');
+            setAnimation('none');
+        } else {
+            setResult(prevResult => prevResult + number);
+        }
+    }
+
+    const onOperator = (symbol) => {
+        console.log('EnterKeyboard.onOperator ' + symbol);
+        if (result !== '?') {
+            if (symbol === 'clear') {
+                if (result.length === 1) {
+                    setFont('grey'); setResult('?');
+                    setAnimation('blinker 5s linear infinite');
+                } else {
+                    setResult(result.slice(0, -1));
+                }
+            } else if (symbol === 'enter') {
+                props.onAnswer(result);
+            }
+        }
+    }
+
+    React.useEffect(() => {
+        if (props.open) {
+            console.log('EnterKeyboard.useEffect');
+            setResult('?'); setAnimation('blinker 5s linear infinite');
+        }
+    }, [props.open, ]);
+
     return (
-        <div className='enter_keyboard'>
-            <button id='digit' onClick={() => props.onDigit('7')}>7</button>
-            <button id='digit' onClick={() => props.onDigit('8')}>8</button>
-            <button id='digit' onClick={() => props.onDigit('9')}>9</button>
+        <div className='enter_keyboard_wrapper'>
+            <div className='enter_keyboard_answer'>
+                <font style={{'color': font, 'animation': animation}}> {result} </font>
+            </div>
+            <div className='enter_keyboard'>
+                <button id='digit' onClick={() => onDigit('7')}>7</button>
+                <button id='digit' onClick={() => onDigit('8')}>8</button>
+                <button id='digit' onClick={() => onDigit('9')}>9</button>
 
-            <button id='digit' onClick={() => props.onDigit('4')}>4</button>
-            <button id='digit' onClick={() => props.onDigit('5')}>5</button>
-            <button id='digit' onClick={() => props.onDigit('6')}>6</button>
+                <button id='digit' onClick={() => onDigit('4')}>4</button>
+                <button id='digit' onClick={() => onDigit('5')}>5</button>
+                <button id='digit' onClick={() => onDigit('6')}>6</button>
 
-            <button id='digit' onClick={() => props.onDigit('1')}>1</button>
-            <button id='digit' onClick={() => props.onDigit('2')}>2</button>
-            <button id='digit' onClick={() => props.onDigit('3')}>3</button>
+                <button id='digit' onClick={() => onDigit('1')}>1</button>
+                <button id='digit' onClick={() => onDigit('2')}>2</button>
+                <button id='digit' onClick={() => onDigit('3')}>3</button>
 
-            <button id='remove' onClick={() => props.onOperator('clear')}>&#60;</button>
-            <button id='zero' onClick={() => props.onDigit('0')}>0</button>
-            <button id='enter' onClick={() => props.onOperator('enter')}>&crarr;</button>
+                <button id='remove' onClick={() => onOperator('clear')}>&#60;</button>
+                <button id='zero' onClick={() => onDigit('0')}>0</button>
+                <button id='enter' onClick={() => onOperator('enter')}>&crarr;</button>
+            </div>
         </div>
     );
 }
