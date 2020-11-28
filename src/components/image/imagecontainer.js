ae347649ad902b1f219c9ec054c5931496ae23f1
Yura Volokitin <yuri.volokitin@gmail.com>
2020-11-28 20:58:39 +0100

    Updates

--- src/components/image/imagecontainer.js
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-import './imagecontainer.css';
-
-const ImageContainer = props => {
-    const aspectRatio = (props.height / props.width) * 100;
-    return (
-        <div className='imagecontainer' style={{ paddingBottom: `${aspectRatio}%` }}/>
-    );
-};
